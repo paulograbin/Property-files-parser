8e287c3b6ab9a40676dff7b5c3dd6e65624803f9
Grabin, Paulo <paulo.grabin@sap.com>
2018-05-07 14:33:22 +0100

    Moving file name filter to another class. Making variables private. Typo.

--- src/main/java/com/paulograbin/propertyanalyser/PropertyanalyserApplication.java
+++ src/main/java/com/paulograbin/propertyanalyser/PropertyanalyserApplication.java
@@ -3,7 +3,6 @@ package com.paulograbin.propertyanalyser;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -13,8 +12,8 @@ import java.util.Map;
 @SpringBootApplication
 public class PropertyanalyserApplication {
 
-    PropertyExtractor propertyExtractor = new PropertyExtractor();
-    EnvironmentLoader environmentLoader = new EnvironmentLoader();
+    private PropertyExtractor propertyExtractor = new PropertyExtractor();
+    private EnvironmentLoader environmentLoader = new EnvironmentLoader();
 
     public static void main(String[] args) {
         if (args.length == 0) {
@@ -34,48 +33,22 @@ public class PropertyanalyserApplication {
         final List<File> filesList = environmentLoader.getFilesList();
 
         for (File file : filesList) {
-            if(shouldScanFile(file)) {
-                System.out.println("Reading properties from file: " + file.getName() + " " + file.getParent());
-                propertyExtractor.processFile(file);
-                environmentCount++;
-            } else {
-                // It's not a local.properties file, so skipping it...
-            }
+            System.out.println("Reading properties from file: " + file.getName() + " " + file.getParent());
+            propertyExtractor.processFile(file);
+            environmentCount++;
         }
 
-        System.out.println(environmentCount + " environment scanned");
+        System.out.println(environmentCount + " environments scanned");
         final Map<String, Property> propertiesDictionary = propertyExtractor.getPropertiesDictionary();
         final Collection<Property> values = propertiesDictionary.values();
 
 
         int finalEnvironmentCount = environmentCount;
-        values.stream().forEach(p -> {
-
+        values.forEach(p -> {
             if(p.getValuePerEnvironment().values().size() == finalEnvironmentCount) {
                 System.out.println("Property " + p.getName() + " is present in all envs with the same value");
                 System.out.println(p.getName());
             }
         });
-    }
-
-    private static boolean shouldScanFile(File file) {
-        return new FilenameFilter() {
-
-            /**
-             * Tests if a specified file should be included in a file list.
-             *
-             * @param dir  the directory in which the file was found.
-             * @param name the name of the file.
-             * @return <code>true</code> if and only if the name should be
-             * included in the file list; <code>false</code> otherwise.
-             */
-            @Override
-            public boolean accept(File dir, String name) {
-                if(name.contains(".properties"))
-                    return true;
-
-                return false;
-            }
-        }.accept(file.getParentFile(), file.getName());
     }
 }
