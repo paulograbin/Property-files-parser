8e0cf1626339befc2ad74491885f51896b2de58d
Grabin, Paulo <paulo.grabin@sap.com>
2018-05-09 00:36:04 +0100

    Extracting property dictionary attribute to main class, instead of property extractor

--- src/main/java/com/paulograbin/propertyanalyser/PropertyanalyserApplication.java
+++ src/main/java/com/paulograbin/propertyanalyser/PropertyanalyserApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +15,9 @@ public class PropertyanalyserApplication {
 
     private PropertyExtractor propertyExtractor = new PropertyExtractor();
     private EnvironmentLoader environmentLoader = new EnvironmentLoader();
+
+    private Map<String, Property> propertiesDictionary = new HashMap<>();
+
 
     public static void main(String[] args) {
         if (args.length == 0) {
@@ -34,14 +38,24 @@ public class PropertyanalyserApplication {
 
         for (File file : filesList) {
             System.out.println("Reading properties from file: " + file.getName() + " " + file.getParent());
-            propertyExtractor.processFile(file);
+            final List<Property> properties = propertyExtractor.processFile(file);
+
+            properties.stream().forEach(this::addToDictionary);
+
             environmentCount++;
         }
 
         System.out.println(environmentCount + " environments scanned");
-        final Map<String, Property> propertiesDictionary = propertyExtractor.getPropertiesDictionary();
-        final Collection<Property> values = propertiesDictionary.values();
+        System.out.println(propertiesDictionary.keySet().size() + " properties found");
+        System.out.println(propertiesDictionary.values()
+                .stream()
+                .mapToInt(p -> p.getValuePerEnvironment()
+                        .keySet()
+                        .size())
+                .sum() + " values found"); // TODO make this less awful to the eyes
 
+
+        final Collection<Property> values = propertiesDictionary.values();
 
         int finalEnvironmentCount = environmentCount;
         values.forEach(p -> {
@@ -50,5 +64,31 @@ public class PropertyanalyserApplication {
                 System.out.println(p.getName());
             }
         });
+    }
+
+    private void addToDictionary(Property property) {
+        try {
+            if (propertyAlreadyProcessed(property)) {
+                appendNewValue(property);
+            } else {
+                addPropertyForTheFirstTime(property);
+            }
+        } catch (RuntimeException e) {
+            System.err.println("Error trying to extract property from line " + property.getOriginalLine() + ". Are you sure this is a property?");
+        }
+    }
+
+    private void appendNewValue(Property property) {
+        final Property currentProp = propertiesDictionary.get(property.getName());
+
+        currentProp.addEnvironmentValue(property.getEnvironmentName(), property.getValue());
+    }
+
+    private boolean propertyAlreadyProcessed(Property property) {
+        return propertiesDictionary.containsKey(property.getName());
+    }
+
+    private void addPropertyForTheFirstTime(Property property) {
+        propertiesDictionary.put(property.getName(), property);
     }
 }
