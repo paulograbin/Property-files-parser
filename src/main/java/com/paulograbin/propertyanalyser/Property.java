84ba6ec5b29df0c7cb73cd03775e5af0e69ae8d7
Grabin, Paulo <paulo.grabin@sap.com>
2018-05-09 00:31:27 +0100

    Adding field to contain the original line where property was extracted from.

--- src/main/java/com/paulograbin/propertyanalyser/Property.java
+++ src/main/java/com/paulograbin/propertyanalyser/Property.java
@@ -9,12 +9,14 @@ public class Property {
     private String name;
     private String environmentName;
     private String value;
+    private String originalLine;
     private Map<String, String> valuePerEnvironment;
 
-    public Property(String name, String environmentName, String value) {
+    public Property(String name, String environmentName, String value, String originalLine) {
         this.name = name;
         this.environmentName = environmentName;
         this.value = value;
+        this.originalLine = originalLine;
 
         this.valuePerEnvironment = new HashMap<>();
     }
@@ -33,6 +35,22 @@ public class Property {
 
     public void addEnvironmentValue(String environmentName, String propertyValue) {
         valuePerEnvironment.put(environmentName, propertyValue);
+    }
+
+    public String getEnvironmentName() {
+        return environmentName;
+    }
+
+    public void setEnvironmentName(String environmentName) {
+        this.environmentName = environmentName;
+    }
+
+    public String getOriginalLine() {
+        return originalLine;
+    }
+
+    public void setOriginalLine(String originalLine) {
+        this.originalLine = originalLine;
     }
 
     @Override
